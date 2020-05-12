4d51a4614281f2ea1abf042da6aab65e5d0e6280
Jake Walden <jwalden@qumulex.com>
2020-05-11 20:38:37 -0400

    simplifying scripts

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cka
 Certified Kubernetes Administrator (CKA)
+
+## Creating the Cluster
+
+### Node Prep
+- Disable swap
+- Install Docker
+- Iptable setup
+- Install Kubeadm
+- Utilities
+
+### Kubeadm Install
+Prerequisites:
+- disable swap
+- disable firewall or open appropriate ports
+- setup host name resolving through /etc/hosts
--- node-prep/node-prep.sh
+++ node-prep/node-prep.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+sudo swapoff -a
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker ubuntu
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
